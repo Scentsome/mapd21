0ea1e34dfe47e690e2df8218ba3d64569401c3d6
Michael Pan <scentsome@gmail.com>
2017-10-11 11:00:02 +0800

    Create new project

---
+++ DelegatePractice/DelegatePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegatePractice
+//
+//  Created by Michael on 11/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
