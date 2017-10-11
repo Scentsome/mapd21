3b9eb0248d122f1a28179d5ece0cfebd9ef5cad9
Michael Pan <scentsome@gmail.com>
2017-10-11 14:02:55 +0800

    Test block and property

---
+++ IIIDemoLong/IIIDemoLong/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  IIIDemoLong
+//
+//  Created by Michael on 11/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ IIIDemoLong/IIIDemoLong/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  IIIDemoLong
+//
+//  Created by Michael on 11/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
