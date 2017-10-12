0d07be916f76189a3cb55c275b49551f0254b6b5
Michael Pan <scentsome@gmail.com>
2017-10-12 09:55:47 +0800

    Run ARSession

--- ARRuler/ARRuler/ViewController.m
+++ ARRuler/ARRuler/ViewController.m
@@ -7,23 +7,71 @@
 //
 
 #import "ViewController.h"
+@import ARKit;
+//#import <ARKit/ARKit.h>
+@import SceneKit;
 
-@interface ViewController ()
 
+@interface ViewController ()<ARSCNViewDelegate>
+@property ARSCNView * sceneView;
+@property UILabel * msgLabel;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSMutableArray * nodes;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    nodes = [@[] mutableCopy];
+    self.sceneView = [[ARSCNView alloc] initWithFrame:CGRectZero];
+    self.sceneView.delegate = self;
+    
+    [self.view addSubview:self.sceneView];
+    
+    self.msgLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.msgLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleTitle1] ;
+    self.msgLabel.textAlignment = NSTextAlignmentCenter ;
+    self.msgLabel.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.msgLabel];
 }
-
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.sceneView.frame = self.view.bounds;
+    self.msgLabel.frame = CGRectMake(0, 16, self.view.bounds.size.width, 64);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    ARWorldTrackingConfiguration * config = [ARWorldTrackingConfiguration new];
+    [self.sceneView.session runWithConfiguration:config options:ARSessionRunOptionResetTracking | ARSessionRunOptionRemoveExistingAnchors];
+    
+}
+-(void)session:(ARSession *)session cameraDidChangeTrackingState:(ARCamera *)camera{
+    NSString * status = @"Loading...";
+    switch (camera.trackingState) {
+        case ARTrackingStateNotAvailable:{
+            status = @"Not Available";
+            break;
+        }
+        case ARTrackingStateLimited:{
+            status = @"Analyzing";
+            break;
+        }
+        case ARTrackingStateNormal:{
+            status = @"Ready";
+            break;
+        }
+        default:
+            status = @"Unknown";
+            break;
+    }
+    self.msgLabel.text = status;
+}
 
 @end
