b61ae306936dda8b9d1daddf8f66399105392657
Michael Pan <scentsome@gmail.com>
2017-10-12 09:25:32 +0800

    Add ARDemo

---
+++ ARRuler/ARRuler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARRuler
+//
+//  Created by Michael on 12/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- SceneDemo/SceneDemo/GameViewController.m
+++ SceneDemo/SceneDemo/GameViewController.m
@@ -8,102 +8,49 @@
 
 #import "GameViewController.h"
 
-@implementation GameViewController
+@interface GameViewController()<SCNSceneRendererDelegate>
+@property SCNView * scnView;
+@property SCNScene * scnScene;
+@property SCNNode * cameraNode;
+@end
+@implementation GameViewController{
+    NSTimeInterval spawnTime  ;
+}
+//NSTimeInterval spawnTime  = 8;
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupView];
+    [self setupScene];
+    [self setupCamera];
+    [self spawnShape];
+     spawnTime = 0.5;
 
-    // create a new scene
-    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
+}
+-(void) setupView{
+    self.scnView = (SCNView *)self.view;
+    self.scnView.delegate = self;
 
-    // create and add a camera to the scene
-    SCNNode *cameraNode = [SCNNode node];
-    cameraNode.camera = [SCNCamera camera];
-    [scene.rootNode addChildNode:cameraNode];
-    
-    // place the camera
-    cameraNode.position = SCNVector3Make(0, 0, 15);
-    
-    // create and add a light to the scene
-    SCNNode *lightNode = [SCNNode node];
-    lightNode.light = [SCNLight light];
-    lightNode.light.type = SCNLightTypeOmni;
-    lightNode.position = SCNVector3Make(0, 10, 10);
-    [scene.rootNode addChildNode:lightNode];
-    
-    // create and add an ambient light to the scene
-    SCNNode *ambientLightNode = [SCNNode node];
-    ambientLightNode.light = [SCNLight light];
-    ambientLightNode.light.type = SCNLightTypeAmbient;
-    ambientLightNode.light.color = [UIColor darkGrayColor];
-    [scene.rootNode addChildNode:ambientLightNode];
-    
-    // retrieve the ship node
-    SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:YES];
-    
-    // animate the 3d object
-    [ship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
-    
-    // retrieve the SCNView
-    SCNView *scnView = (SCNView *)self.view;
-    
-    // set the scene to the view
-    scnView.scene = scene;
-    
-    // allows the user to manipulate the camera
-    scnView.allowsCameraControl = YES;
-        
-    // show statistics such as fps and timing information
-    scnView.showsStatistics = YES;
-
-    // configure the view
-    scnView.backgroundColor = [UIColor blackColor];
-    
-    // add a tap gesture recognizer
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
-    NSMutableArray *gestureRecognizers = [NSMutableArray array];
-    [gestureRecognizers addObject:tapGesture];
-    [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
-    scnView.gestureRecognizers = gestureRecognizers;
 }
 
-- (void) handleTap:(UIGestureRecognizer*)gestureRecognize
-{
-    // retrieve the SCNView
-    SCNView *scnView = (SCNView *)self.view;
-    
-    // check what nodes are tapped
-    CGPoint p = [gestureRecognize locationInView:scnView];
-    NSArray *hitResults = [scnView hitTest:p options:nil];
-    
-    // check that we clicked on at least one object
-    if([hitResults count] > 0){
-        // retrieved the first clicked object
-        SCNHitTestResult *result = [hitResults objectAtIndex:0];
-        
-        // get its material
-        SCNMaterial *material = result.node.geometry.firstMaterial;
-        
-        // highlight it
-        [SCNTransaction begin];
-        [SCNTransaction setAnimationDuration:0.5];
-        
-        // on completion - unhighlight
-        [SCNTransaction setCompletionBlock:^{
-            [SCNTransaction begin];
-            [SCNTransaction setAnimationDuration:0.5];
-            
-            material.emission.contents = [UIColor blackColor];
-            
-            [SCNTransaction commit];
-        }];
-        
-        material.emission.contents = [UIColor redColor];
-        
-        [SCNTransaction commit];
-    }
+- (void) setupCamera {
+    self.cameraNode = [SCNNode new];
+    self.cameraNode.camera = [SCNCamera new];
+    self.cameraNode.position = SCNVector3Make(0.0, 0.0, 10.0);
+    [self.scnScene.rootNode addChildNode:self.cameraNode];
 }
+
+-(void) setupScene{
+    self.scnScene = [SCNScene new];
+    self.scnView.scene = self.scnScene;
+    self.scnScene.background.contents = [UIImage imageNamed:@"Background_Diffuse"];
+    
+}
+
+
 
 - (BOOL)shouldAutorotate
 {
@@ -114,14 +61,6 @@
     return YES;
 }
 
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
 
 - (void)didReceiveMemoryWarning
 {
@@ -129,4 +68,63 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
+- (void) spawnShape {
+    
+    SCNGeometry * geometry = [SCNGeometry geometry];
+    
+    switch (arc4random_uniform(8)) {
+        case 0:
+            geometry = [SCNSphere sphereWithRadius:0.5];
+            break;
+        case 1:
+            geometry = [SCNBox boxWithWidth:1.0 height:1.0 length:1.0 chamferRadius:0];
+            break;
+        case 2:
+            geometry = [SCNPyramid pyramidWithWidth:1.0 height:1.0 length:1.0];
+            break;
+        case 3:
+            geometry = [SCNTorus torusWithRingRadius:0.5 pipeRadius:0.25];
+            break;
+        case 4:
+            geometry = [SCNCapsule capsuleWithCapRadius:0.3 height:2.5];
+            break;
+        case 5:
+            geometry = [SCNCylinder cylinderWithRadius:0.3 height:2.5];
+            break;
+        case 6:
+            geometry = [SCNCone coneWithTopRadius:0.25 bottomRadius:0.5 height:1.0];
+            break;
+        case 7:
+            geometry = [SCNTube tubeWithInnerRadius:0.25 outerRadius:0.5 height:1.0];
+            break;
+        default:
+            break;
+    }
+    SCNNode *geometerNode = [SCNNode nodeWithGeometry:geometry];
+    
+    
+    geometerNode.physicsBody = [SCNPhysicsBody bodyWithType:SCNPhysicsBodyTypeDynamic shape:nil];
+    NSInteger X = (NSInteger)(arc4random_uniform(5)) - 2;
+    NSInteger Y = 10 + arc4random() % (18 - 10 + 1);
+    SCNVector3 force = SCNVector3Make(8, 0, 0);
+    SCNVector3 position = SCNVector3Make(0.05, 0.05, 0.05);
+    [geometerNode.physicsBody applyForce:force atPosition:position impulse:true];
+    
+    UIColor *color = [UIColor colorWithRed: arc4random_uniform(256)/255.0
+                                     green: arc4random_uniform(256)/255.0
+                                      blue: arc4random_uniform(256)/255.0
+                                     alpha: 1.0];
+    geometry.materials.firstObject.diffuse.contents = color;
+    [self.scnScene.rootNode addChildNode:geometerNode];
+}
+
+- (void)renderer:(id<SCNSceneRenderer>)renderer updateAtTime:(NSTimeInterval)time{
+    if (time > spawnTime) {
+        
+        [self spawnShape];
+        spawnTime = time + 0.5;
+    }
+
+//    [self spawnShape];
+}
 @end
