6eac7cd79d1e08357fe5efc11f13520d47b0d619
Michael Pan <scentsome@gmail.com>
2017-10-12 11:50:48 +0800

    add predication codes

---
+++ IIIDemoLong/IIIDemoLong/view/DrawPad.m
@@ -0,0 +1,21 @@
+//
+//  DrawPad.m
+//  IIIDemoLong
+//
+//  Created by Michael on 12/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import "DrawPad.h"
+
+@implementation DrawPad
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ PredictImage/PredictImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PredictImage
+//
+//  Created by Michael on 12/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
