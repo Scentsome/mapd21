443b5c757874e67ec9421c8264be2093f76665a3
Michael Pan <scentsome@gmail.com>
2017-10-13 16:18:45 +0800

    Collection View

---
+++ CollectionViewDemo/CollectionViewDemo/CellItem.h
@@ -0,0 +1,13 @@
+//
+//  CellItem.h
+//  CollectionViewDemo
+//
+//  Created by Michael on 13/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CellItem : NSObject
+
+@end
---
+++ CollectionViewDemo/CollectionViewDemo/CellItem.m
@@ -0,0 +1,13 @@
+//
+//  CellItem.m
+//  CollectionViewDemo
+//
+//  Created by Michael on 13/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import "CellItem.h"
+
+@implementation CellItem
+
+@end
