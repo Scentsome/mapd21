7fe4b7923c78b4addc152691e6aeb14d0481639b
Michael Pan <scentsome@gmail.com>
2017-10-16 10:33:36 +0800

    Init MyBook

---
+++ MyBook/MyBook/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  MyBook
+//
+//  Created by Michael on 16/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ MyBook/MyBook/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MyBook
+//
+//  Created by Michael on 16/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
