ba330269cd9e16f249c82aee061ac83cf0f5ecb6
Michael Pan <scentsome@gmail.com>
2017-10-16 13:53:29 +0800

    Create Plot Demo Project

---
+++ MyPlot/MyPlot/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyPlot
+//
+//  Created by Michael on 16/10/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
