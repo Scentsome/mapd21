a64adb862c19df2dd225d4ed1b7cc66ec6f28f5e
Michael Pan <scentsome@gmail.com>
2017-10-16 14:52:56 +0800

    Add PieChartView

--- MyPlot/MyPlot/ViewController.m
+++ MyPlot/MyPlot/ViewController.m
@@ -7,23 +7,82 @@
 //
 
 #import "ViewController.h"
+@import Charts;
 
-@interface ViewController ()
+@interface ViewController ()<ChartViewDelegate>
+@property (weak, nonatomic) IBOutlet PieChartView *pieChartView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSString * name;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.pieChartView.noDataText = @"請加入資料";
+    name = @"Michael";
+    self.pieChartView.delegate = self;
+    [self prepareData];
 }
 
+- (void) prepareData {
+    NSDictionary * pieChartRawData = @{@"iOS 11":@40, @"iOS 10":@46, @"iOS 9":@9, @"Earlier":@5};
+    
+    NSMutableArray * yValues = [NSMutableArray array];
+    
+//    [pieChartRawData enumerateKeysAndObjectsUsingBlock:^(NSString *   key, NSNumber *   obj, BOOL *  stop) {
+//        PieChartDataEntry * entry = [[PieChartDataEntry alloc] initWithValue:[obj doubleValue] label:key ];
+//        [yValues addObject:entry];
+//    }];
+    
+    for( NSString * key in pieChartRawData.allKeys ){
+        NSNumber * obj = pieChartRawData[key];
+        PieChartDataEntry * entry = [[PieChartDataEntry alloc] initWithValue:[obj doubleValue] label:key ];
+        [yValues addObject:entry];
+    }
+    
+    PieChartDataSet *dataSet = [[PieChartDataSet alloc] initWithValues:yValues label:@"iOS Distribution"];
+    
+    NSMutableArray *colors = [[NSMutableArray alloc] init];
+    [colors addObjectsFromArray:[ChartColorTemplates joyful]];
+    dataSet.colors = colors;
+    
+    
+    PieChartData *pieChartData = [[PieChartData alloc] initWithDataSet:dataSet];
+    
+    NSNumberFormatter *pFormatter = [[NSNumberFormatter alloc] init];
+    pFormatter.numberStyle = NSNumberFormatterPercentStyle;
+    pFormatter.maximumFractionDigits = 1;
+    pFormatter.multiplier = @1.f;
+    pFormatter.percentSymbol = @"%";
+    
+    [pieChartData setValueFormatter:[[ChartDefaultValueFormatter alloc] initWithFormatter:pFormatter]];
+
+    self.pieChartView.data = pieChartData;
+    [pieChartData setValueTextColor:[UIColor blueColor]];
+    
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(void)chartValueSelected:(ChartViewBase *)chartView entry:(ChartDataEntry *)entry highlight:(ChartHighlight *)highlight{
+    NSLog(@"Did select %@",entry);
+}
+-(void)chartValueNothingSelected:(ChartViewBase *)chartView{
+    NSLog(@"Nothing selected");
+}
+- (IBAction)highlight:(id)sender {
+    [self.pieChartView highlightValueWithX:2 y:0 dataSetIndex:0];
+//    [self.];
+//    [UIView animateWithDuration:1.0 animations:^{
+        [self.pieChartView setRotationAngle:45];
+//    }];
+    
+}
 
 @end
